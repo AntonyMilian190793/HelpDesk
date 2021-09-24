4d46b1b14e2089fe1daf1b7210b808dbc88253ba
Antony190793 <antonymilian19@gmail.com>
2021-09-24 02:13:51 -0500

    Commit 25-09-2021

--- controller/ticket.php
+++ controller/ticket.php
@@ -147,6 +147,10 @@
                 echo json_encode($output);
             }   
         break;  
+
+        case "insertdetalle":
+            $ticket->insert_ticketdetalle($_POST["tick_id"], $_POST["usu_id"], $_POST["tickd_descrip"]);
+        break;    
     }
 
 ?>    
--- view/DetalleTicket/detalleticket.js
+++ view/DetalleTicket/detalleticket.js
@@ -4,19 +4,22 @@ function init() {
 
 $(document).ready(function(){
     var tick_id = getUrlParameter('ID');
-    $.post("../../controller/ticket.php?op=listardetalle", { tick_id: tick_id}, function(data){
-        $('#lbldetalle').html(data);
-    });
 
+    listardetalle(tick_id);
     $.post("../../controller/ticket.php?op=mostrar", { tick_id: tick_id}, function(data){
         data = JSON.parse(data);
         $('#lblestado').html(data.tick_estado);
         $('#lblnomusuario').html(data.usu_nom + ' ' + data.usu_ape);
         $('#lblfechcrea').html(data.fech_crea);
+
+        $('#lblnomidticket').html("Detalle Ticket " + data.ticket_id);
+        $('#cat_nom').val(data.cat_nom);
+        $('#tick_titulo').val(data.tick_titulo);
+        $('#tickd_descripusu').summernote('code', data.tick_descrip);
     });
 
     $('#tickd_descrip').summernote({
-        height: 150,
+        height: 400,
         lang: "es-ES",
         callbacks: {
             onImageUpload: function(image) {
@@ -28,6 +31,13 @@ $(document).ready(function(){
             }
         }
     });
+
+    $('#tickd_descripusu').summernote({
+        height: 400,
+        lang: "es-ES",
+    });
+
+    $('#tickd_descripusu').summernote("disable");
 });
 
 
@@ -45,5 +55,27 @@ var getUrlParameter = function getUrlParameter(sParam) {
         }
     }
 };
+
+$(document).on("click", "#btnenviar", function(){
+    var tick_id = getUrlParameter('ID'); 
+    var usu_id = $('#user_idx').val();
+    var tickd_descrip = $('#tickd_descrip').val();
+    $.post("../../controller/ticket.php?op=insertdetalle", { tick_id: tick_id, usu_id:usu_id, tickd_descrip:tickd_descrip}, function(data){
+        listardetalle(tick_id);
+        $('#tickd_descrip').summernote('reset');
+        swal("Correcto!", "Registrado Correctamente", "success");
+    });    
+});
+
+
+$(document).on("click", "#btncerrarticket", function(){
+    
+});
+
+function listardetalle(tick_id){
+    $.post("../../controller/ticket.php?op=listardetalle", { tick_id: tick_id}, function(data){
+        $('#lbldetalle').html(data);
+    });
+}
 
 init();
